118032b0f4297984cfbedae097e473f01289abcd
Michelle Wee <michellew1822@gmail.com>
2024-11-04 19:09:37 -0800

    Created skeleton for event boxes

---
+++ TritonTurnUp/src/components/events/Event.jsx
@@ -0,0 +1,18 @@
+import React, { useState} from 'react'
+import './Event.css'
+
+function Event() {
+  
+    return (
+
+        <div className="event_box">
+
+            This is an event box.
+
+        </div>
+
+    );
+
+}
+
+export default Event;
--- TritonTurnUp/src/components/pages/Home.jsx
+++ TritonTurnUp/src/components/pages/Home.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import Navbar from '../navbar/Navbar'
+import Event from '../events/Event';
+
 
 const Home = () => {
   return (
     <div>
       <Navbar />
       <h1>Home</h1>
+      <Event />
     </div>
   )
 }
