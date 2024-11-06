22b9cf7ad96ea1fcd7f1c52551e739e49626a47f
Michelle Wee <michellew1822@gmail.com>
2024-11-05 17:47:58 -0800

    adjusting design based on page size

--- TritonTurnUp/src/components/events/Event.jsx
+++ TritonTurnUp/src/components/events/Event.jsx
@@ -1,8 +1,8 @@
 import React, { useState} from 'react'
 import './Event.css'
-import rejectImage from './X circle.png';
-import linkImage from './Link.png';
-import acceptImage from './Check square.png';
+import rejectImage from '../../assets/X circle.png';
+import linkImage from '../../assets/Link.png';
+import acceptImage from '../../assets/Check square.png';
 
 function Event() {
   
@@ -10,65 +10,84 @@ function Event() {
 
         <div className="event_box">
 
-            <div className="event_header">
-                <p>
-                    Event Title
-                </p>
-            </div>
+            <div className = "grid_elements_list">
 
-            <div className="flex_label_list">
-                <div className = "flex_label_item">
-                    <div className = "label">Label</div>
-                </div>
-                <div className = "flex_label_item">
-                    <div className = "label">Label</div>
-                </div>
-                <div className = "flex_label_item">
-                    <div className = "label">Label</div>
-                </div>
-            </div>
+                <div className = "grid_element">
 
-            <div className="event_information_box"> 
-                <p>Information about event (is scrollable).</p>
-            </div>
+                    <div className="event_header">
+                        <p>
+                            Event Title
+                        </p>
+                    </div>
 
-            <div className="flex_image_list">
-                <div className = "flex_image_item">
+                </div>
+
+                <div className = "grid_element">
+
+                    <div className="flex_label_list">
+                        
+                        <div className = "flex_label_item">
+                            <div className = "label">Label</div>
+                        </div>
+                        <div className = "flex_label_item">
+                            <div className = "label">Label</div>
+                        </div>
+                        <div className = "flex_label_item">
+                            <div className = "label">Label</div>
+                        </div>
+                        <div className = "flex_label_item">
+                            <div className = "label">Label</div>
+                        </div>
+
+                    </div>
+
+                </div>
+
+                <div className = "grid_element">
+
+                    <div className="event_information_box"> 
+                        <p>
+                            Information about event (is scrollable).
+                        </p>
+                    </div>
+
+                </div>
+
+                <div className = "grid_element">
+
                     <img src="https://pbs.twimg.com/media/FgRkpDCXoAED48L.jpg" />
-                </div>
-                <div className = "flex_image_item">
-                    <img src="https://pbs.twimg.com/media/FgRkpDCXoAED48L.jpg" />
-                </div>
-                <div className = "flex_image_item">
-                    <img src="https://pbs.twimg.com/media/FgRkpDCXoAED48L.jpg" />
-                </div>
-                <div className = "flex_image_item">
-                    <img src="https://pbs.twimg.com/media/FgRkpDCXoAED48L.jpg" />
-                </div>
-            </div>
 
-            <div className = "flex_button_bar">
-                <div className = "button">
-                    <button class="button"> 
-                        <img src={rejectImage}/>
-                    </button>
                 </div>
 
-                <div className = "button">
-                    <button class="button"> 
-                        <img src={linkImage}/>
-                    </button>
+                <div className = "grid_element">
+                
+                    <div className = "flex_button_bar">
+
+                        <div className = "button">
+                            <button class="button"> 
+                                <img src={rejectImage}/>
+                            </button>
+                        </div>
+
+                        <div className = "button">
+                            <button class="button"> 
+                                <img src={linkImage}/>
+                            </button>
+                        </div>
+
+                        <div className = "button">
+                            <button class="button"> 
+                                <img src={acceptImage}/>
+                            </button>
+                        </div>
+
+                    </div>
+
                 </div>
 
-                <div className = "button">
-                    <button class="button"> 
-                        <img src={acceptImage}/>
-                    </button>
-                </div>
             </div>
 
         </div>
-
 
     );
 
