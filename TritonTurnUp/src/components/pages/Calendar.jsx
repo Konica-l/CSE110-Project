1e6b1bcd5e2002212836156869e32a13fd8afffc
Konica-l <batmaan3000@gmail.com>
2024-11-10 00:11:46 -0800

    Update Calendar.jsx

--- TritonTurnUp/src/components/pages/Calendar.jsx
+++ TritonTurnUp/src/components/pages/Calendar.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
 import Navbar from '../navbar/Navbar'
+import Calendar from 'react-calendar'
 
-const Calendar = () => {
+const CalendarConstruct = () => {
   return (
     <div>
       <Navbar />
       <h1>Calendar</h1>
+      <div id = 'wrap'>
+        <Calendar
+                    minDate = {new Date(2024, 0,1)}
+                    maxDate = {new Date(2025, 11,1)}
+                    minDetail = "year"
+                    showNeighboringMonth = {false}
+          />
+      </div>
     </div>
   )
 }
 
-export default Calendar
+export default CalendarConstruct
