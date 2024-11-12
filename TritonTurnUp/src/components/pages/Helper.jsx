0e8f36eda9768c3faa12096cd659e8530184449f
Konica-l <batmaan3000@gmail.com>
2024-11-11 21:50:14 -0800

    Updated Helper function to lookup by id. Need work on tags

--- TritonTurnUp/src/components/pages/Helper.jsx
+++ TritonTurnUp/src/components/pages/Helper.jsx
@@ -3,18 +3,31 @@ import Navbar from '../navbar/Navbar'
 import data from '../../event_list.json';
 
 const Helper = () => {
-  const ind = getEvent(0);
+    const Event = getEventByID("ea4b76321377e2961e4afc6825b36169");
     return (
       <div>
         <Navbar />
         <h1>Helper</h1>
-        <p>{ind.id}</p>
+        <p>{Event.id}</p>
+        <img src = {Event.image}></img>
       </div>
     )
   }
 
-export function getEvent(index) { //returns event 
-  return (data[index])
+export function getEventByID(id) { //returns event 
+  for (var i = 0; i < data.length; i++) {
+    if (data[i].id === id) {
+      return data[i];
+    }
+  } 
+  return console.error("id does not exist");
 }
+
+export function tagLookUp(tags) {
+  const similar = 0;
+  
+  
+}
+
 
 export default Helper
