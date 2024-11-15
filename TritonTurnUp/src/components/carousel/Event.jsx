eb9ce58c4eac9efb419cc5881e91279253db4ba8
nicshrly <130081492+nicshrly@users.noreply.github.com>
2024-11-14 17:19:10 -0800

    modified event card styling

--- TritonTurnUp/src/components/carousel/Event.jsx
+++ TritonTurnUp/src/components/carousel/Event.jsx
@@ -6,16 +6,23 @@ import acceptImage from '../../assets/Check square.png'
 
 function Event(props){
   return (
-    <div className='card'>
-      <div className='container'>
-        <h1>{props.title}</h1>
+    <div className='event-card'>
+      <div className='event-card-container'>
+        <p className='title'>{props.title}</p>
+        <div className='img-crop'>
+          <img className='img' src={props.img} alt={props.title}/>
+        </div>
         <p className='date'>{props.date}</p>
+        <ul className='tags'>
+          <li>tag 1</li>
+          <li>tag 2</li>
+          <li>tag 3</li>
+        </ul>
         <p className='preview'>{props.text}</p>
-        <img className='info-img' src={props.img} alt={props.title}/>
         <div className='buttons'>
-          <button><img src={rejectImage}/></button>
-          <button><img src={linkImage}/></button>
-          <button><img src={acceptImage}/></button>
+          <button><img src={rejectImage} alt='Reject'/></button>
+          <button><img src={linkImage} alt='More'/></button>
+          <button><img src={acceptImage} alt='Accept'/></button>
         </div>
       </div>
     </div>
