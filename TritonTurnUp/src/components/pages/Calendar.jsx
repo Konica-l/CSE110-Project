f0f663999b5fbceb6641426eb50e2587b19cd9af
Konica-l <batmaan3000@gmail.com>
2024-11-18 23:40:09 -0800

    New Iteration of Calendar.jsx

--- TritonTurnUp/src/components/pages/Calendar.jsx
+++ TritonTurnUp/src/components/pages/Calendar.jsx
@@ -1,11 +1,45 @@
-import React, {useState} from 'react'
 import Navbar from '../navbar/Navbar'
-import {Calendar, momentLocalizer} from 'react-big-calendar'
+import {Calendar, Views, momentLocalizer} from 'react-big-calendar'
+import {useMemo, useState} from 'react'
+import events from 'events'
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 import moment from 'moment'
 
 
+
+/*export default function CalendarConstruct({
+  localizer = mlocalizer,
+  ...props
+}) {
+  const {views} = useMemo( 
+    () => ({
+      components: {
+        timeSlotWrapper: ColoredDateCellWrapper,
+      },
+      views: Object.keys(Views).map((k) => Views[k]),
+    }),
+    []
+  )
+}
+return (
+    <div>
+      <Navbar />
+      <h1>Calendar</h1>
+      <div id = "calendar">
+        <Calendar
+            localizer = {mlocalizer}
+            events = {events}
+            startAccessor= "start"
+            endAccessor= "end"
+            views = {views}
+            style = {{ height: 800 }}
+          />
+      </div>
+    </div>
+)*/
+
 const localizer = momentLocalizer(moment)
+
 
 const CalendarConstruct = (props) => (
     <div>
---
+++ TritonTurnUp/src/components/pages/events.jsx
@@ -0,0 +1,24 @@
+export default [
+    /* {
+      id: 0,
+      title: 'All Day Event very long title',
+      allDay: true,
+      start: new Date(2015, 3, 0),
+      end: new Date(2015, 3, 1),
+    }, */
+    {
+      id: 1,
+      title: 'Long Event',
+      start: new Date(2024, 11, 7),
+      end: new Date(2024, 11, 10),
+    },
+  
+    {
+      id: 2,
+      title: 'DTS STARTS',
+      start: new Date(2024, 11, 13, 0, 0, 0),
+      end: new Date(2024, 11, 20, 0, 0, 0),
+    },
+  
+
+]
