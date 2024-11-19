4d7462768cc5a8f0aba7df29f9b5f1c28e1310eb
Michelle Wee <michellew1822@gmail.com>
2024-11-18 18:56:16 -0800

    Fixed img conflict

--- TritonTurnUp/src/components/profileElements/ProfileBar.jsx
+++ TritonTurnUp/src/components/profileElements/ProfileBar.jsx
@@ -7,7 +7,7 @@ const ProfileBar = () => {
         <div>
             <h1 className = "heading"> My Profile: </h1>
             <h2 className = "heading">(Username)</h2>
-            <img src="https://ih1.redbubble.net/image.5179670953.2221/flat,750x,075,f-pad,750x1000,f8f8f8.jpg" />
+            <img className= "userimg" src="https://ih1.redbubble.net/image.5179670953.2221/flat,750x,075,f-pad,750x1000,f8f8f8.jpg" />
             <h2 className = "heading"> My Events:</h2>
             <EventSummary />
         </div>
