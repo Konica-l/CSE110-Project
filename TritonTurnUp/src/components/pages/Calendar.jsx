2af00d36a1b755433b9120dda491e3cc0a6816b2
Konica-l <batmaan3000@gmail.com>
2024-11-19 12:05:31 -0800

    Deleted react-calendar, updated Calendar
    Calendar has been changed.

--- TritonTurnUp/src/components/pages/Calendar.jsx
+++ TritonTurnUp/src/components/pages/Calendar.jsx
@@ -1,9 +1,12 @@
 import Navbar from '../navbar/Navbar'
-import {Calendar, Views, momentLocalizer} from 'react-big-calendar'
-import {useMemo, useState} from 'react'
-import events from 'events'
+import {Calendar, Views, dayjsLocalizer} from 'react-big-calendar'
+import {Fragment, useMemo, useState} from 'react'
+import myEvents from './events'
 import 'react-big-calendar/lib/css/react-big-calendar.css';
-import moment from 'moment'
+import dayjs from 'dayjs'
+import timezone from 'dayjs/plugin/timezone'
+dayjs.extend(timezone)
+
 
 
 /*export default function CalendarConstruct({
@@ -36,24 +39,42 @@ return (
       </div>
     </div>
 )*/
+const ColoredDateCellWrapper = ({ children }) =>
+  React.cloneElement(React.Children.only(children), {
+    style: {
+      backgroundColor: 'lightblue',
+    },
+  })
 
-const localizer = momentLocalizer(moment)
+const localizer = dayjsLocalizer(dayjs)
 
+export default function Dayjs({ ...props }) {
+  const { components,defaultDate, max } = useMemo(
+    () => ({
+      components: {
+        timeSlotWrapper: ColoredDateCellWrapper,
+      },
+      defaultDate: new Date().toLocaleDateString(),
+      max: dayjs().endOf('day').subtract(1,'hours').toDate(),
+    }),
+    []
+  )
 
-const CalendarConstruct = (props) => (
-    <div>
+  return (
+    <Fragment>
       <Navbar />
-      <h1>Calendar</h1>
-      <div id = "calendar">
+      <div className='height600' {...props}> 
         <Calendar
-            localizer = {localizer}
-            //events = {myEventsList}
-            startAccessor= "start"
-            endAccessor= "end"
-            style = {{ height: 800 }}
-          />
+          components={components}
+          defaultDate={defaultDate}
+          events={myEvents}
+          localizer = {localizer}
+          max={max}
+          showMultiDayTimes
+          style = {{height: 600}}
+          step={60}
+        />
       </div>
-    </div>
-)
-
-export default CalendarConstruct
+    </Fragment>
+  )
+}
--- TritonTurnUp/src/components/pages/events.jsx
+++ TritonTurnUp/src/components/pages/events.jsx
@@ -1,3 +1,5 @@
+const now = new Date()
+//MONTHS ARE ZERO INDEXED
 export default [
     /* {
       id: 0,
@@ -9,16 +11,21 @@ export default [
     {
       id: 1,
       title: 'Long Event',
-      start: new Date(2024, 11, 7),
-      end: new Date(2024, 11, 10),
+      start: new Date(2024, 10, 7),
+      end: new Date(2024, 10, 10),
     },
   
     {
       id: 2,
       title: 'DTS STARTS',
-      start: new Date(2024, 11, 13, 0, 0, 0),
-      end: new Date(2024, 11, 20, 0, 0, 0),
+      start: new Date(2024, 10, 19),
+      end: new Date(2024, 10, 20),
     },
-  
+    {
+      id: 14,
+      title: 'Today',
+      start: new Date(new Date().setHours(new Date().getHours() - 3)),
+      end: new Date(new Date().setHours(new Date().getHours() + 3)),
+    },
 
 ]
