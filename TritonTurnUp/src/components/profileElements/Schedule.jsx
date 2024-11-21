41bb5bd80aaf5e274445ad39d631d9508c8ef97c
Michelle Wee <michellew1822@gmail.com>
2024-11-21 02:03:56 -0800

    Creating calendar

--- TritonTurnUp/src/components/profileElements/Schedule.jsx
+++ TritonTurnUp/src/components/profileElements/Schedule.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import './Schedule.css';
+import Day from "./ScheduleDay.jsx";
 
 function Schedule(){
 
     return (
         
-        <div>Schedule/calendar should go here</div>
+        <div>
+            <h1 className = "heading">My schedule: </h1>
+            <div className = "dayContainer">
+                <Day> Monday</Day>
+                <Day>Tuesday</Day>
+                <Day>Wednesday</Day>
+                <Day>Thursday</Day>
+                <Day>Friday</Day>
+                <Day>Saturday</Day>
+                <Day>Sunday</Day>
+            </div>
+
+        </div>
     
     )
 
---
+++ TritonTurnUp/src/components/profileElements/ScheduleDay.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import './ScheduleDay.css';
+
+function ScheduleDay(){
+
+    return (
+        
+        <div className = "Day">
+            <h2> Day of the Week: </h2>
+            <h2> Mornings ~ 8:00am-11:59am: </h2>
+            <h3 className= "time">8:00:</h3>
+            <div className= "overlay">Event 1 (8:00-10:00)</div>
+            <div class="horizontal-line"></div>
+            <h3 className= "time">9:00:</h3>
+            <div class="horizontal-line"></div>
+            <h3 className= "time">10:00:</h3>
+            <div class="horizontal-line"></div>
+            <h3 className= "time">11:00:</h3>
+            <div class="horizontal-line"></div>
+
+            <h2> Afternoons ~ 12:00pm-4:59pm: </h2>
+            <h3 className= "time">12:00:</h3>
+            <div class="horizontal-line"></div>
+            <h3 className= "time">1:00:</h3>
+            <div class="horizontal-line"></div>
+            <h3 className= "time">2:00:</h3>
+            <div class="horizontal-line"></div>
+            <h3 className= "time">3:00:</h3>
+            <div class="horizontal-line"></div>
+            <h3 className= "time">4:00:</h3>
+            <div class="horizontal-line"></div>
+
+            <h2> Evenings ~ 5:00pm-11:59pm: </h2>
+            <h3 className= "time">5:00:</h3>
+            <div class="horizontal-line"></div>
+            <h3 className= "time">6:00:</h3>
+            <div class="horizontal-line"></div>
+            <h3 className= "time">7:00:</h3>
+            <div class="horizontal-line"></div>
+            <h3 className= "time">8:00:</h3>
+            <div class="horizontal-line"></div>
+            <h3 className= "time">9:00:</h3>
+            <div class="horizontal-line"></div>
+            <h3 className= "time">10:00:</h3>
+
+        </div>
+    
+    )
+
+}
+  
+export default ScheduleDay
